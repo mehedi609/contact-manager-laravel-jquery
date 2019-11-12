e2ee6d8affe739316c8038f70683ab1aeb3041ce
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2019-11-12 21:14:52 +0600

    Created migrations for groups and contacts table

---
+++ database/migrations/2019_11_12_150803_create_contacts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('company')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->bigInteger('group_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('group_id')
+              ->references('id')
+              ->on('groups')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
